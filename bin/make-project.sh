2ef1c9907f43193934ee89d4595f0cbb4427abb0
straup <straup@gmail.com>
2011-09-01 01:25:26 +0200

    account for abs path of make-project script; create project bin directory; generate secrets

--- bin/make-project.sh
+++ bin/make-project.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-WHOAMI=$0
+WHOAMI=`readlink -f $0`
 WHEREAMI=`dirname $WHOAMI`
+TOOLS=`dirname $WHEREAMI`
 
 PROJECT=$1
 
@@ -19,7 +20,17 @@ rm -rf ${PROJECT}/www/cron
 rm -rf ${PROJECT}/www/docs
 rm -rf ${PROJECT}/www/tests
 
+cp ${PROJECT}/www/include/config.php.example ${PROJECT}/www/include/config.php
+
 mv ${PROJECT}/www/schema ${PROJECT}/
 
 # sudo chown -R www-data ${PROJECT}/www/templates_c
 
+cp -r ${TOOLS}/flamework-bin ${PROJECT}/bin
+
+# generate a bunch of secrets
+# TODO: squirt these in to the config file automatically
+
+php -q ${PROJECT}/bin/generate_secret.php
+php -q ${PROJECT}/bin/generate_secret.php
+php -q ${PROJECT}/bin/generate_secret.php
