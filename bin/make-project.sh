5c60ecf2a43f788d4cb4d9b48f1b6834685a2467
straup <straup@gmail.com>
2012-01-21 19:34:39 +0100

    copy README in to project apache dir

--- bin/make-project.sh
+++ bin/make-project.sh
@@ -31,6 +31,7 @@ cp ${PROJECT}/www/include/config.php.example ${PROJECT}/www/include/config.php
 # cp -r ${TOOLS}/flamework-bin ${PROJECT}/bin
 
 cp ${TOOLS}/apache/example.conf ${PROJECT}/apache/${PROJECT_NAME}.conf.example
+cp ${TOOLS}/apache/example.conf ${PROJECT}/apache/README.md
 
 # in case the user is running out of a sub-directory
 
