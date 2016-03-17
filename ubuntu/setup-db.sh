99bb630a311a93ea229de7a28860e2ed1e97873c
thisisaaroland <aaron@mapzen.com>
2016-03-16 18:17:54 -0700

    more better db setup

--- ubuntu/setup-db.sh
+++ ubuntu/setup-db.sh
@@ -61,9 +61,9 @@ mysql -u root -p < /tmp/${DBNAME}.sql
 
 unlink /tmp/${DBNAME}.sql
 
-perl -p -i -e "s/GLOBALS\['cfg'\]\['db_main'\]\['pass'\] = ''/GLOBALS\['cfg'\]\['db_main'\]\['pass'\] = '${PASSWORD}'/" ${SECRETS};
-perl -p -i -e "s/GLOBALS\['cfg'\]\['db_users'\]\['pass'\] = ''/GLOBALS\['cfg'\]\['db_users'\]\['pass'\] = '${PASSWORD}'/" ${SECRETS};
-perl -p -i -e "s/GLOBALS\['cfg'\]\['db_poormans_slaves'\]\['pass'\] = ''/GLOBALS\['cfg'\]\['db_poormans_slaves'\]\['pass'\] = '${PASSWORD}'/" ${SECRETS};
+perl -p -i -e "s/GLOBALS\['cfg'\]\['db_main'\]\['pass'\] = '[^']*'/GLOBALS\['cfg'\]\['db_main'\]\['pass'\] = '${PASSWORD}'/" ${SECRETS};
+perl -p -i -e "s/GLOBALS\['cfg'\]\['db_users'\]\['pass'\] = '[^']*'/GLOBALS\['cfg'\]\['db_users'\]\['pass'\] = '${PASSWORD}'/" ${SECRETS};
+perl -p -i -e "s/GLOBALS\['cfg'\]\['db_poormans_slaves'\]\['pass'\] = '[^']*'/GLOBALS\['cfg'\]\['db_poormans_slaves'\]\['pass'\] = '${PASSWORD}'/" ${SECRETS};
 
 # please to update db name and db user name in config.php...
 # (20160316/thisisaaronland)
